a3dea15280ec714c02d76519929cbcb45fc31157
Adam Cobb <cobb.2326@gmail.com>
2018-07-29 14:10:12 -0700

    Created Job

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,9 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.job("oncePerSecond", function(request, status) {
+  // the params passed through the start request
+  const params = request.params;
+  // Headers from the request that triggered the job
+  const headers = request.headers;
+
+  status.message("sucessfully ran the job");
 });
